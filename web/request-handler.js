e530ff13c275a51693470e1a0af1042687d7c80c
Hack Reactor Student <student@hackreactor.com>
2017-09-15 19:12:04 -0700

    loaded html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,27 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('http');
+var fs = require('fs');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function (req, response) {
+
+  if (req.method === 'GET' && req.url === '/'){
+    console.log('hello')//public/index.html');
+    fs.readFile('./web/public/index.html', 'utf8',function(err, data){
+      if (err){
+        throw err;
+      }
+
+      response.writeHeader(200, {"Content-type": "text/html"});
+      console.log('data:', data);
+      //response.write()
+      response.end(data);
+done()
+    });
+  }
+  console.log(archive.paths.siteAssets+'/index.html');
+  //res.end(archive.paths.list);
+
+
 };
